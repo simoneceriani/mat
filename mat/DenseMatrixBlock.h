6072eabc7f6bb61dc250916366a13693ca5d29a9
CERIANI Simone <simone.ceriani@ec.europa.eu>
2020-12-20 11:24:39 +0100

    add iterator to DenseMatrixBlock by creating subclass DenseMatrixBlockIterable

--- mat/DenseMatrixBlock.h
+++ mat/DenseMatrixBlock.h
@@ -5,11 +5,12 @@
 #include <vector>
 
 #include "MatrixBlockBase.h"
+#include "SparsityPattern.h"
 
 namespace mat {
 
   template<class T, int BR, int BC, int NBR = mat::Dynamic, int NBC = mat::Dynamic >
-  class DenseMatrixBlock final : public MatrixBlockBase<BR, BC, NBR, NBC>  {
+  class DenseMatrixBlock : public MatrixBlockBase<BR, BC, NBR, NBC> {
 
   public:
 
@@ -39,7 +40,7 @@ namespace mat {
 
     virtual ~DenseMatrixBlock();
 
-    void resize(const BlockDescriptor& blockDesc) override final;
+    virtual void resize(const BlockDescriptor& blockDesc) override final;
 
 
     const StorageType& mat() const {
@@ -52,15 +53,15 @@ namespace mat {
 
     inline BlockType block(int r, int c) {
       return _mat.block<RowTraits::blockSizeAtCompileTime, ColTraits::blockSizeAtCompileTime>(
-        this->rowBlockStart(r), this->colBlockStart(r),
-        this->rowBlockSize(r), this->colBlockSize(r)
+        this->rowBlockStart(r), this->colBlockStart(c),
+        this->rowBlockSize(r), this->colBlockSize(c)
         );
     }
 
     inline ConstBlockType block(int r, int c) const {
       return _mat.block<RowTraits::blockSizeAtCompileTime, ColTraits::blockSizeAtCompileTime>(
-        this->rowBlockStart(r), this->colBlockStart(r),
-        this->rowBlockSize(r), this->colBlockSize(r)
+        this->rowBlockStart(r), this->colBlockStart(c),
+        this->rowBlockSize(r), this->colBlockSize(c)
         );
     }
 
@@ -68,5 +69,181 @@ namespace mat {
 
   };
 
+  template<class T, int Ordering, int BR, int BC, int NBR = mat::Dynamic, int NBC = mat::Dynamic >
+  class DenseMatrixBlockIterable final : public DenseMatrixBlock<T, BR, BC, NBR, NBC> {
 
+  public:
+
+    using DenseMatrixBlockT = DenseMatrixBlock<T, BR, BC, NBR, NBC>;
+
+    using Traits = typename DenseMatrixBlockT::Traits;
+
+    using StorageType = typename DenseMatrixBlockT::StorageType;
+    using SubMatrixType = typename DenseMatrixBlockT::SubMatrixType;
+    using BlockType = typename DenseMatrixBlockT::BlockType;
+    using ConstBlockType = typename DenseMatrixBlockT::ConstBlockType;
+
+    using BlockDescriptor = typename DenseMatrixBlockT::BlockDescriptor;
+    using DimensionDescriptorRow = typename DenseMatrixBlockT::DimensionDescriptorRow;
+    using DimensionDescriptorCol = typename DenseMatrixBlockT::DimensionDescriptorCol;
+
+    using RowTraits = typename DenseMatrixBlockT::RowTraits;
+    using ColTraits = typename DenseMatrixBlockT::ColTraits;
+
+  private:
+    std::vector<int> _outerStarts;  // size = outersize
+    std::vector<int> _innerIndexes; // size = nnz blocks
+    std::vector<int> _uid2outer;  // size = nnz blocks, return the outer given uid
+
+    void createPattern(const SparsityPattern<Ordering>& sp);
+    
+    inline int row(int outer, int inner) const {
+      if (Ordering == mat::ColMajor) {
+        return inner;
+      }
+      else if (Ordering == mat::RowMajor) {
+        return outer;
+      }
+      else {
+        ASSERT_FALSE();
+      }
+    }
+
+    inline int col(int outer, int inner) const {
+      if (Ordering == mat::ColMajor) {
+        return outer;
+      }
+      else if (Ordering == mat::RowMajor) {
+        return inner;
+      }
+      else {
+        ASSERT_FALSE();
+      }
+    }
+  public:
+    DenseMatrixBlockIterable();
+    DenseMatrixBlockIterable(const BlockDescriptor& blockDesc, const SparsityPattern<Ordering>& sp);
+
+    virtual ~DenseMatrixBlockIterable();
+
+    void resize(const BlockDescriptor& blockDesc, const SparsityPattern<Ordering>& sp);
+
+    int nonZeroBlocks() const {
+      return int(_innerIndexes.size());
+    }
+
+    int blockUID(int r, int c) const;
+
+    bool hasBlock(int r, int c) {
+      return blockUID(r, c) >= 0;
+    }
+
+    inline BlockType blockByUID(int uid) {
+      int out = _uid2outer[uid];
+      int in = uid - _outerStarts[out];
+      int r = this->row(out, in);
+      int c = this->col(out, in);
+      return this->blockOuterInner(out, in, r, c);
+    }
+
+    inline ConstBlockType blockByUID(int uid) const {
+      int out = _uid2outer[uid];
+      int in = uid - _outerStarts[out];
+      int r = this->row(out, in);
+      int c = this->col(out, in);
+      return this->blockOuterInner(out, in, r, c);
+    }
+
+    // iterator on inner dimension 
+    template<class BaseT>
+    class InnerIterator {
+      int _outer;
+
+      BaseT* _sm;
+      int _curId;
+      int _lastId;
+
+    public:
+      InnerIterator(BaseT& _sm, int outer, int curId, int lastId);
+      virtual ~InnerIterator();
+
+      inline int operator()() const {
+        assert(_curId <= _lastId);
+        return _curId;
+      }
+
+      inline int blockUID() const {
+        assert(_curId <= _lastId);
+        return _curId;
+      }
+
+      inline int end() const {
+        return _lastId;
+      }
+
+      inline void operator ++() {
+        assert(_curId <= _lastId);
+        _curId++;
+      }
+      inline void operator ++(int /*trash*/) {
+        assert(_curId <= _lastId);
+        _curId++;
+      }
+
+
+      int row() const {
+        assert(_curId <= _lastId);
+        int inner = _sm->_innerIndexes[_curId];
+        return _sm->row(_outer, inner);
+      }
+
+
+      int col() const {
+        assert(_curId <= _lastId);
+        int inner = _sm->_innerIndexes[_curId];
+        return _sm->col(_outer, inner);
+      }
+
+      template<typename RetType = BlockType>
+      inline std::enable_if_t<!std::is_const<BaseT>::value, RetType> block() {
+        assert(_curId <= _lastId);
+        int inner = _sm->_innerIndexes[_curId];
+        int r = _sm->row(_outer, inner);
+        int c = _sm->col(_outer, inner);
+        return _sm->block(r, c);
+      }
+
+      inline ConstBlockType block() const {
+        assert(_curId <= _lastId);
+        int inner = _sm->_innerIndexes[_curId];
+        int r = _sm->row(_outer, inner);
+        int c = _sm->col(_outer, inner);
+        return _sm->block(r, c);
+      }
+
+
+    };
+
+    // iterate on column blocks (if block are stored in col major)
+    template<typename RetType = InnerIterator<DenseMatrixBlockIterable>>
+    std::enable_if_t<IsColMajor<Ordering>::value, RetType> colBegin(int c) {
+      return InnerIterator<DenseMatrixBlockIterable>(*this, c, _outerStarts[c], _outerStarts[c + 1]);
+    }
+
+    template<typename RetType = const InnerIterator<const DenseMatrixBlockIterable>>
+    std::enable_if_t<IsColMajor<Ordering>::value, RetType> colBegin(int c) const {
+      return InnerIterator<const DenseMatrixBlockIterable>(*this, c, _outerStarts[c], _outerStarts[c + 1]);
+    }
+
+    // iterate on row blocks (if block are stored in row major)
+    template<typename RetType = InnerIterator<DenseMatrixBlockIterable>>
+    std::enable_if_t<IsRowMajor<Ordering>::value, RetType> rowBegin(int r) {
+      return InnerIterator<DenseMatrixBlockIterable>(*this, r, _outerStarts[r], _outerStarts[r + 1]);
+    }
+
+    template<typename RetType = InnerIterator<const DenseMatrixBlockIterable>>
+    std::enable_if_t<IsRowMajor<Ordering>::value, RetType> rowBegin(int r) const {
+      return InnerIterator<const DenseMatrixBlockIterable>(*this, r, _outerStarts[r], _outerStarts[r + 1]);
+    }
+  };
 }
--- mat/DenseMatrixBlock.hpp
+++ mat/DenseMatrixBlock.hpp
@@ -3,6 +3,7 @@
 
 #include "MatrixBlockDescriptor.hpp"
 #include "MatrixBlockBase.hpp"
+#include "Utils.hpp"
 
 namespace mat {
 
@@ -32,6 +33,105 @@ namespace mat {
   template< class T, int BR, int BC, int NBR, int NBC >
   void DenseMatrixBlock<T, BR, BC, NBR, NBC>::setZero() {
     _mat.setZero();
+  }
+
+
+  //--------------------------------------------------------------------------------------------------
+
+  template< class T, int Ordering, int BR, int BC, int NBR, int NBC >
+  DenseMatrixBlockIterable<T, Ordering, BR, BC, NBR, NBC>::DenseMatrixBlockIterable() 
+    : DenseMatrixBlock<T, BR, BC, NBR, NBC>() 
+  {
+    auto sp = SparsityPattern<Ordering>(this->numBlocksRow(), this->numBlocksCol());
+    this->createPattern(sp);
+  }
+
+
+  template< class T, int Ordering, int BR, int BC, int NBR, int NBC >
+  DenseMatrixBlockIterable<T, Ordering, BR, BC, NBR, NBC>::DenseMatrixBlockIterable(const BlockDescriptor& blockDesc, const SparsityPattern<Ordering>& sp) 
+    : DenseMatrixBlock <T, BR, BC, NBR, NBC>(blockDesc)
+  {
+    if (blockDesc.numBlocksRow() != 0 && blockDesc.numBlocksCol() != 0) {
+      this->createPattern(sp);
+    }
+  }
+
+  template< class T, int Ordering, int BR, int BC, int NBR, int NBC >
+  DenseMatrixBlockIterable<T, Ordering, BR, BC, NBR, NBC>::~DenseMatrixBlockIterable() {
+
+  }
+
+  template< class T, int Ordering, int BR, int BC, int NBR, int NBC >
+  void DenseMatrixBlockIterable<T, Ordering, BR, BC, NBR, NBC>::resize(const BlockDescriptor& blockDesc, const SparsityPattern<Ordering>& sp) {
+    DenseMatrixBlock<T, BR, BC, NBR, NBC>::resize(blockDesc);
+    this->createPattern(sp);
+  }
+
+  template< class T, int Ordering, int BR, int BC, int NBR, int NBC >
+  void DenseMatrixBlockIterable<T, Ordering, BR, BC, NBR, NBC>::createPattern(const SparsityPattern<Ordering>& sp) {
+    // init block structure
+    _innerIndexes.resize(sp.count());
+    _uid2outer.resize(sp.count());
+    _outerStarts.resize(sp.outerSize() + 1);
+    int count = 0;
+    for (int o = 0; o < sp.outerSize(); o++) {
+      _outerStarts[o] = count;
+      for (int in : sp.inner(o)) {
+        _innerIndexes[count] = in;
+        _uid2outer[count] = o;
+        count++;
+      }
+    }
+    _outerStarts[sp.outerSize()] = count;
+    assert(count == sp.count());
+
+  }
+
+  template< class T, int Ordering, int BR, int BC, int NBR, int NBC >
+  int DenseMatrixBlockIterable<T, Ordering, BR, BC, NBR, NBC>::blockUID(int r, int c) const {
+    if (Ordering == mat::RowMajor) {
+      int start = _outerStarts[r];
+      int end = _outerStarts[r + 1];
+
+      auto itS = _innerIndexes.begin() + start;
+      auto itE = _innerIndexes.begin() + end;
+
+      auto id = Utils::binary_search(itS, itE, c);
+      if (id == itE) return -1;
+      else return id - _innerIndexes.begin();
+    }
+    else if (Ordering == mat::ColMajor) {
+      int start = _outerStarts[c];
+      int end = _outerStarts[c + 1];
+
+      auto itS = _innerIndexes.begin() + start;
+      auto itE = _innerIndexes.begin() + end;
+
+      auto id = Utils::binary_search(itS, itE, r);
+      if (id == itE) return -1;
+      else return id - _innerIndexes.begin();
+    }
+    else {
+      ASSERT_FALSE();
+    }
+    return -1;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------------------------------
+
+  template< class T, int Ordering, int BR, int BC, int NBR, int NBC >
+  template<class BaseT>
+  DenseMatrixBlockIterable<T, Ordering, BR, BC, NBR, NBC>::InnerIterator<BaseT>::InnerIterator(BaseT& sm, int outer, int curId, int lastId)
+    : _outer(outer), _sm(&sm), _curId(curId), _lastId(lastId)
+  {
+
+  }
+
+  template< class T, int Ordering, int BR, int BC, int NBR, int NBC >
+  template<class BaseT>
+  DenseMatrixBlockIterable<T, Ordering, BR, BC, NBR, NBC>::InnerIterator<BaseT>::~InnerIterator()
+  {
+
   }
 
 
